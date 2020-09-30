dfd28adc106f9d1f99ff39ac7d64740b97c0836a
carefree-window-8613 <jakobep24@gmail.com>
2020-09-30 21:10:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board
+  "   |   |   "
+  "-----------"
+  "   |   |   "
+  "-----------"
+  "   |   |   "
+end 
+
 display_board(board)
